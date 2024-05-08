b653cc20bcbf6b642a300c6e74e557c175e97ef6
MikaelDv <enzomikael@hotmail.com>
2024-05-08 09:55:33 -0300

    aula manipulação objetos

--- js/script.js
+++ js/script.js
@@ -1,66 +1,151 @@
-// console.log("Olá, mundo!");
+//JavaScript básico tópicos
+// console.log("Olá Mundo!");
 
+// //Capturar o botão pelo ID e atrelar a ele um evento de click.
 // const btnElemento = document.getElementById("botao");
 // btnElemento.addEventListener("click", function(){
-//     console.log("Clicaram no meu butao!!")
+//     console.log("Botão testado.");
 // });
 
-// document.getElementById("botao").addEventListener("click", function(){
-//     console.log("Clicaram no meu butao!!")
-// })
-
+//1.Declaração de variáveis
 // var nome = "João";
 // let sobrenome = "Silva";
-// console.log("Nome completo: " + nome + " " + sobrenome);
+//  console.log("Nome completo : " + nome +" "+ sobrenome);
+// //2.Declaração de constantes
 // const idade = 20;
-// console.log("Idade: " + idade)
+// console.log("Idade : " + idade);
+// idade = 21;
 
-// Hoisting:
+//2.Hoisting
+//Diferença entre var e let
 // var nome = "João";
-// let sobrenome = "Silva";
 
 // if(true){
-//     let nome = "Marcos"
+//     let nome = "Silva";
 // }
 
-// console.log(nome)
+// console.log(nome);
 
-// if(true){
-//     let nome = "Marcos"
-//     console.log(nome)
-// }
+//3.Tipos de dados
+    
+// let tiposDeDados = [
+//     "Number",
+//     "String",
+//     "Boolean",
+//     "Null",
+//     "Undefined",
+//     "Object",
+//     "Array",
+//     "Function"
+// ]
 
-// Tipos de Dados:
-let tiposDeDados = [
-    "Number", 
-    "String", 
-    "Boolean", 
-    "Null", 
-    "Undefined", 
-    "Object", 
-    "Array", 
-    "Function"
-]
+// //Imprimindo o array com console.table.
+// console.table(tiposDeDados);
 
 
-//Recuperando a lista ul através do id
-let listaTiposDeDados = document.getElementById("tipo-dados");
+// //4.Operadores
+ 
+// //5.Estruturas condicionais
 
-tiposDeDados.forEach((tipo)=>{
-    //Criando um elemento <li> para inserir na <ul>
-    let itemLista = document.createElement("li");
-    //Adicionando o texto do tipo de dado
-     itemLista.innerText = tipo;
-     //Inserindo o elemento <li> na <ul>
-     listaTiposDeDados.appendChild(itemLista);
-});
+// //6.Estruturas de repetição
+// // //Imprimindo Array com forEach com função anônima
+// // tiposDeDados.forEach(function(tipo){
+// //     console.log(tipo);
+// // });
+// // //Imprimindo Array com forEach com arrowFunction
+// // tiposDeDados.forEach((tipo)=>{
+// //     console.log(tipo);
+// // });
+// //Itera sobre um array de tipos de dados que serão inseridos em uma lista ul no html de id tipo-dados
+// //Recuperando a lista ul através do id
+// let listaTiposDeDados = document.getElementById("tipo-dados");
 
 // tiposDeDados.forEach((tipo)=>{
-//     console.log(tipo);
-// });
-//               ==^
-// tiposDeDados.forEach(function(tipo){
-//     console.log(tipo);
+//     //Criando um elemento <li> para inserir na <ul>
+//     let itemLista = document.createElement("li");
+//     //Adicionando o texto do tipo de dado
+//      itemLista.innerText = tipo;
+//      //Inserindo o elemento <li> na <ul>
+//      listaTiposDeDados.appendChild(itemLista); 
 // });
 
-// console.table(tiposDeDados)
+//7.Funções
+
+// function soma(a,b){
+//     // let a = 4;
+//     // let b = 5;
+
+//     if((a+b)>=10){
+//         console.log(`A soma  de a: ${a} e b: ${b} é maior ou igual à 10.`);
+//         return true;
+//     }else{
+//         console.log(`A soma  de a: ${a} e b: ${b} não é maior do que 10.`);
+//         return false;
+//     }
+// }
+
+// const soma = (a,b)=>{
+ 
+//     if((a+b)>=10){
+//         console.log(`A soma  de a: ${a} e b: ${b} é maior ou igual à 10.`);
+//         return true;
+//     }else{
+//         console.log(`A soma  de a: ${a} e b: ${b} não é maior do que 10.`);
+//         return false;
+//     }
+// }
+
+//Recuperar os campos email e senha através de querySelector.
+
+// const inputEmail = document.querySelector("#idEmail");
+// const inputSenha = document.querySelector("#idSenha");
+
+function validacao(inputEmail,inputSenha) {
+    console.log(inputEmail.value);
+        usuario = {
+            email : inputEmail.value,
+            senha : inputSenha.value
+        }
+        
+        let{email, senha} = usuario;
+        if((email == "") && (senha == "")){
+            alert("O preenchimento dos campos são obrigatórios.");
+            return false;
+        }
+
+        usuario = {...usuario, nomeCompleto : "Enzo Mikael"}
+        return true;
+    }
+    
+    
+    
+    
+//8.Arrays
+
+
+
+// //9.Objetos
+// let usuario = {
+//     emailUsuario : "jo@email.com",
+//     senhaUsuario : "123456",
+//     nomeCompleto : "João da Silva"
+// }
+// // console.log(usuario.nomeCompleto + ", você está logado com o seu email: "+ usuario.emailUsuario);
+
+// // let propsVariavel = "emailUsuario";
+// // console.log(usuario["nomeCompleto"] + ", você está logado com o seu email: "+ usuario[propsVariavel]);
+// console.log(usuario);
+
+// //OPERADOR SPREAD( ... )
+// usuario = {...usuario, idade:20 , regiao:"São Paulo"};
+
+// console.log(usuario);
+
+
+// //Destructuring
+// const{emailUsuario,idade,regiao}=usuario;
+// console.log(`E-mail do Usuário: ${emailUsuario}`);
+// console.log(`Idade do Usuário: ${idade}`);
+// console.log(`Região de Residência: ${regiao}`);
+
+//10.Funções de array
